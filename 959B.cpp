6b95bfd5ccdd3f9c5882901a0f267545a261fea3
[ashikul] <[2024ashikul@gmail.com]>
2024-11-01 18:57:58 +0600

    initializing the solution file of 959B

---
+++ 959B.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int n;
+        cin >> n ;
+        vector<int> inputs(n,1);
+        
+
+        
+    }
+    
+
+    return 0;
+}
